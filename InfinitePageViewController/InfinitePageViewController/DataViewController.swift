db21e80d59799284e07a181bf5ecc71e557c7c24
htomcat <thamada@yahoo-corp.jp>
2018-04-14 12:15:35 +0900

    [add] Initial Commit

---
+++ InfinitePageViewController/InfinitePageViewController/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  InfinitePageViewController
+//
+//  Created by htomcat on 2018/04/14.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
